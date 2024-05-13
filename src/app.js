8829b06957edbaa7bdb637260392d0d2e8df4d0b
ReesvGupta <guptareesav16@gmail.com>
2024-05-13 18:49:37 +0530

    fix bug

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json({ limit: "20kb" }))
 app.use(express.urlencoded({ extended: true, limit: "20kb" }))
 app.use(express.static("public"))
-app.use(cors())
+app.use(cors({ origin: "*" }))
 
 const userRouter = require("../src/routes/user.route.js")
 const contactRouter = require("../src/routes/contact.route.js")
